538d95b19ac2709acdd9a9b11beaa1ab303c31a9
AmurimGabriel <amurim.souza27@gmail.com>
2021-03-09 19:03:31 -0300

    Create PersonController and H2 Database test

---
+++ person-api/src/main/java/com/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,18 @@
+package com.digitalinnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+	@GetMapping
+	public String getBook() {
+
+		new PersonController();
+
+		return "Teste";
+	}
+}
