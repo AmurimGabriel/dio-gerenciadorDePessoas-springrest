2d7b17894fae611194762e89b9ec3b45f50c05a6
AmurimGabriel <amurim.souza27@gmail.com>
2021-03-10 09:26:33 -0300

    Develope DELETE Person by ID

--- person-api/src/main/java/com/digitalinnovation/personapi/exception/PersonNotFoundException.java
+++ person-api/src/main/java/com/digitalinnovation/personapi/exception/PersonNotFoundException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class PersonNotFoundException extends Exception {
+	private static final long serialVersionUID = 8975602564411397406L;
 
 	public PersonNotFoundException(Long id) {
 		super("Person not found with ID: " + id);
